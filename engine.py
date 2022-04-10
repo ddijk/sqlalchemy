6d3fa29ab2729ed81114f08659045b7524f53eb4
Dick Dijk <dick@dijkrosoft.nl>
2022-04-10 18:21:02 +0200

    example of insert

--- engine.py
+++ engine.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy import text
+from sqlalchemy.orm import Session
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
 
 with engine.connect() as conn:
@@ -11,12 +12,15 @@ with engine.connect() as conn:
     conn.commit()
 
 with engine.connect() as conn:
-    result = conn.execute(text("SELECT x, y FROM some_table"))
+    conn.execute(
+        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+        [{"x": 11, "y": 12}, {"x": 13, "y": 14}]
+    )
+    conn.commit()
+
+
+with engine.connect() as conn:
+    result = conn.execute(
+        text("SELECT x, y FROM some_table WHERE y > :y"), {"y": 2})
     for row in result:
         print(f"x: {row.x}  y: {row.y}")
-
-
-
-
-
-
